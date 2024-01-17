93b7164ee6bdb4eb51a8e75bb6c25bac675e7426
rafaelRizzoPVOX <rafaelbreschi4@gmail.com>
2024-01-17 20:44:24 -0300

    ajustes SEO

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,9 +1,16 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head />
+      <meta name="description" content="Seu sistema para integração com a API do WhatsApp para mensagens automáticas." />
+      <meta name="keywords" content="API do WhatsApp, Mensagens automáticas, Integração WhatsApp" />
+      <meta name="author" content="Rafael Rizzo Breschi" />
+
+      <title>Chama lá!</title>
+
       <body>
         <Main />
         <NextScript />
